8463280c2fde9aaf3c56a0402cd526f0c87947b3
alidenumarov <um_aliden_97@mail.ru>
2016-02-08 18:18:21 +0600

    added prime numbers

---
+++ prime.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab121
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string s = Console.ReadLine();
+            int n = int.Parse(s);
+
+            int[] array = new int[n];
+            for (int i = 0; i < n; ++i)
+            {
+                array[i] = int.Parse(Console.ReadLine());
+            }
+
+            for (int i = 0; i < n; ++i)
+            {
+                bool prime = true;
+                for (int j = 2; j * j <= array[i]; ++j)
+                    if (array[i] % j == 0)
+                        prime = false;
+                if (prime == true && array[i] != 1)
+                {
+                    Console.WriteLine(array[i]);
+                }
+            }
+            Console.ReadKey();
+        }
+    }
+}
